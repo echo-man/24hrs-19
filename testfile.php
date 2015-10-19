6a3f491b729b090ed3e3998be739bce2fb51a600
echo-man <gema.buj@gmail.com>
2015-10-19 13:23:10 +0700

    Categorized SQL connection configuration file (example and TRY IT )

--- testfile.php
+++ testfile.php
@@ -1,13 +1,37 @@
-<!--=============================-->
-<!--Connection configuration file-->
-<!--=============================-->
+<!--========================================-->
+<!--Example of Connection configuration file-->
+<!--========================================-->
 
 <?php
 
 	define("MYSQLUSER", "php24sql");
 	define("MYSQLPASS", "hJQV8RTe5t");
 	define("HOSTNAME", "localhost");
+
 	$connection = @new mysqli(HOSTNAME, MYSQLUSER, MYSQLPASS);
+
+	if ($connection->connect_error) {
+		die('Connect Error: ' . $connection->connect_error);
+	} else {
+		echo 'Successful connection to MySQL';
+}
+
+?>
+
+
+<!--========================================-->
+<!--TRY IT Connection configuration file-->
+<!--========================================-->
+
+<?php
+
+	define("MYSQLUSER", "php24sql");
+	define("MYSQLPASS", "hJQV8RTe5t");
+	define("HOSTNAME", "localhost");
+	define("MYSQLDB", "smithside_backup");
+
+	$connection = @new mysqli(HOSTNAME, MYSQLUSER, MYSQLPASS);
+
 	if ($connection->connect_error) {
 		die('Connect Error: ' . $connection->connect_error);
 	} else {
