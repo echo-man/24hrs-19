eac0b093a70d2ae9202513372c3341585438be79
echo-man <gema.buj@gmail.com>
2015-10-19 15:16:21 +0700

    SQL connection using object oriented style

---
+++ testfile4.php
@@ -0,0 +1,30 @@
+<!--=========================================-->
+<!--TRY IT Connection configuration file-->
+<!--Connecting to database "smithside_backup"-->
+<!--and show its table using SHOW TABLES-->
+<!--Object Oriented Style of mysqli-->
+<!--=========================================-->
+
+<?php
+
+	define("MYSQLUSER", "php24sql");
+	define("MYSQLPASS", "hJQV8RTe5t");
+	define("HOSTNAME", "localhost");
+	define("MYSQLDB", "smithside_backup");
+
+	$connection = @new mysqli(HOSTNAME, MYSQLUSER, MYSQLPASS, MYSQLDB);
+
+	if ($connection->connect_error) {
+		die('Connect Error: ' . $connection->connect_error);
+	} else {
+		echo 'Congratulations! Successful connection to MySQL <br/>';
+		if ($result = $connection->query("SHOW TABLES")) {
+			$count = $result->num_rows;
+			echo 'Tables: ' .($count) .'<br />';
+			while ($row = $result->fetch_array()) {
+				echo $row[0] .'<br />';
+			}
+		}
+	}
+
+?>
